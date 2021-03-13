67ef729c67ee76a634f1d9d96d82adaa69933094
Isaac <isaacgeazy@gmail.com>
2021-03-13 00:18:51 -0300

    Atividade Graus Fahrenheit para Celsius

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace GrausFC
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string entrada;
+            double celsius, fare;
+
+              Console.WriteLine("----- Conversão de Fahrenheit para Celsius -----");
+              Console.WriteLine();
+              Console.WriteLine("valor em Fahrenheit: ");
+              entrada = Console.ReadLine();
+
+              fare = Convert.ToDouble(entrada);
+
+              celsius = (fare - 32) / 1.8;
+
+              Console.WriteLine();
+              Console.WriteLine($"A conversão de Fahrenheit para Celsius é {celsius}");
+
+        }
+    }
+}
